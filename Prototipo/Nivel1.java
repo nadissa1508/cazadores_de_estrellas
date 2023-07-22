3a9b8805aaaefbf5997e5a16558ae1a6abfd5da6
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2023-07-21 18:11:32 -0600

    Corazones de vida para Nave1
    Estos corazones representan la cantidad de vida que tiene la Nave1

--- Prototipo/Nivel1.java
+++ Prototipo/Nivel1.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Nivel1 extends World
 {
-
+    Nave1 nave1 = new Nave1();
+    private int numCorazones;
+    private int maxCorazones;
     /**
      * Constructor for objects of class Nivel1.
      * 
@@ -21,10 +23,39 @@ public class Nivel1 extends World
     }
     
     private void prepare(){
-        Nave1 nave1=new Nave1();
+        
         addObject(nave1,130,275);
         Villano1 villano = new Villano1();
         addObject(villano,850,275);
+        maxCorazones = 3;
+        numCorazones = maxCorazones;
+        
+        for (int i = 0; i < maxCorazones; i++) {
+            vidaNave corazon = new vidaNave();
+            addObject(corazon, 50 + i * 80, 50);
+        }
+    }
+    
+    public void act(){
+        int vidaNave = nave1.getHealth();
+        int corazonesMostrados = Math.min(maxCorazones, (vidaNave + 25) / 50); 
+        if (corazonesMostrados != numCorazones) {
+            updateCorazones(corazonesMostrados);
+        }
+    }
+    
+    private void updateCorazones(int corazonesMostrados) {
+        // Ocultar los corazones que ya no deben mostrarse
+        for (int i = maxCorazones - 1; i >= corazonesMostrados; i--) {
+            vidaNave corazon = (vidaNave) getObjects(vidaNave.class).get(i);
+        }
+        
+        // Mostrar los corazones que deben mostrarse
+        for (int i = 0; i < corazonesMostrados; i++) {
+            vidaNave corazon = (vidaNave) getObjects(vidaNave.class).get(i);
+        }
+        
+        numCorazones = corazonesMostrados;
     }
 }
 
